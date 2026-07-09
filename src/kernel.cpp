3537d4c6615b6136050cd585bf091906b5ac2d5e
nishanthedau <fuz.nishant@gmail.com>
2026-07-09 06:43:03 +0000

    addition of printf and update in make

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,5 +1,14 @@
 // #include "stdio.h"
 
+void printf(char* str){
+    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+    for(int i=0; str[i] != '\0'; i++){
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+
+}
+
 void kernelMain(void* multiboot_structure, unsigned int magicnumber){
 
     printf("test bzz\n");
