7b5ff270a247cb79ab2e0930311b75facf59af12
nishanthedau <fuz.nishant@gmail.com>
2026-07-10 04:25:25 +0000

    addition of  newline and few bug tweaks

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,18 +1,48 @@
 // #include "stdio.h"
 
-void printf(char* str){
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+static int x = 0;
+static int y = 0;
+
+void printf(const char *str){
+    unsigned short *VideoMemory = (unsigned short *)0xb8000;
 
     for(int i=0; str[i] != '\0'; i++){
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-    }
+        if(str[i] == '\n'){
+            x = 0;
+            y++;
 
+            if(y >= 25)
+                y = 0;
+            continue;
+        }
+
+        VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xFF00) | str[i];
+        x++;
+
+        if(x >= 80){
+            x = 0;
+            y++;
+
+            if(y >= 25)
+                y = 0;
+        }
+        // VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+}
+
+// minor fix to avoid bugs - ensure proper initialization
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors(){
+    for(constructor* i = &start_ctors; i != &end_ctors; i++){
+        (*i)();
+    }
 }
 
 // extern cuz we dont want it to be renamed by compiler
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber){
-
+extern "C" void kernelMain(void *multiboot_structure, unsigned int /*multiboot_magic*/){
     printf("test bzz\n");
-    while(1);
-
+    printf("testok\n");
+    while (1);
 }
